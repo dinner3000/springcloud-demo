e0bb3b24a30612dbb35a52b3a381a7a0a9fb2c95
Gary Zhu <dinner3000@hotmail.com>
2018-07-16 21:38:23 +0800

    Add hysteria dashboard

--- comboservice1/src/main/java/com/dinner3000/demo/comboservice/ComboserviceApplication.java
+++ comboservice1/src/main/java/com/dinner3000/demo/comboservice/ComboserviceApplication.java
@@ -3,9 +3,11 @@ package com.dinner3000.demo.comboservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 @RestController
 public class ComboserviceApplication {
 
--- comboservice2/src/main/java/com/dinner3000/demo/comboservice/ComboserviceApplication.java
+++ comboservice2/src/main/java/com/dinner3000/demo/comboservice/ComboserviceApplication.java
@@ -5,17 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableHystrixDashboard
 @RestController
 public class ComboserviceApplication {
 
