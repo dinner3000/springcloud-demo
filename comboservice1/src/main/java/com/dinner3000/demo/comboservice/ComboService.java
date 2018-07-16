9b735826c4413e959fc125a902e6b212c3bc2da5
Zhu Jiawei <ab052151zhujiawei@ab-insurance.com>
2018-07-16 16:35:47 +0800

    Add Hystrix

---
+++ comboservice1/src/main/java/com/dinner3000/demo/comboservice/ComboService.java
@@ -0,0 +1,29 @@
+package com.dinner3000.demo.comboservice;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ComboService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    public Object doubleCall() throws Exception {
+        List<String> resultList = new ArrayList<>();
+//        throw new Exception("Expecting error");
+        resultList.add(restTemplate.getForObject("http://service1/service", String.class));
+        resultList.add(restTemplate.getForObject("http://service2/service", String.class));
+        return org.apache.commons.lang.StringUtils.join(resultList, " | ");
+    }
+
+    protected Object fallback(Throwable e) {
+
+        return e.getMessage();
+    }
+}
--- comboservice1/src/main/java/com/dinner3000/demo/comboservice/ComboserviceApplication.java
+++ comboservice1/src/main/java/com/dinner3000/demo/comboservice/ComboserviceApplication.java
@@ -5,18 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 @RestController
 public class ComboserviceApplication {
 
@@ -24,20 +21,18 @@ public class ComboserviceApplication {
 		SpringApplication.run(ComboserviceApplication.class, args);
 	}
 
-	@Bean
-	@LoadBalanced
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	@Autowired
-	RestTemplate restTemplate;
+    @Autowired
+    ComboService comboService;
 
 	@RequestMapping("/service")
-	public Object comboService1(){
-		List<String> resultList = new ArrayList<>();
-		resultList.add(restTemplate.getForObject("http://service1/service", String.class));
-		resultList.add(restTemplate.getForObject("http://service2/service", String.class));
-		return org.apache.commons.lang.StringUtils.join(resultList, " | ");
+	public Object comboService1() throws Exception {
+	    return comboService.doubleCall();
 	}
+
 }
